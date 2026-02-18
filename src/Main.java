44620fe68db44ad72f504a527311202f7728fd70
Joanna Jacob <joannajacob910@gmail.com>
2026-02-18 14:13:54 +0530

    use case 1 done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+//Author: Joanna Jacob
+// version 1.0
+
 public class Main {
     public static void main(String[] args){
-
+        System.out.println("Welcome to the Palindrome Checker Managemnet System");
+        System.out.println("Version: 1.0");
+        System.out.println("System. initialized successfully");
     }
 }
