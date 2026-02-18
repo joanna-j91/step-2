46465365572c026e08b8701f6898587d42758dbe
Joanna Jacob <joannajacob910@gmail.com>
2026-02-18 15:05:47 +0530

    completed use case 4

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,23 @@
 //Author: Joanna Jacob
-// version 3.0
+// version 4.0
 import java.util.Scanner;
+class Palindrome{
+    public static boolean checker(String s){
+        char[] string = s.toCharArray();
+        int low = 0, high = string.length-1;
+        while(low <= high){
+            if(string[low] != string[high]){
+                return false;
+            }
+            low++;
+            high--;
+        }
+        return true;
+    }
+}
 
 public class Main {
     public static void main(String[] args){
-        Scanner input = new Scanner(System.in);
-        System.out.println("Enter a string: ");
-
-        String og_string = input.nextLine();
-        og_string = og_string.toLowerCase();
-
-        String reversed = "";
-
-        for(int i=og_string.length()-1; i>=0; i--){
-            reversed = reversed + og_string.charAt(i);
-        }
-        if(og_string.equals(reversed)){
-            System.out.println(og_string + " is a palindrome");
-        }
-        else{
-            System.out.println(og_string + " is not a palindrome");
-        }
+        System.out.println(Palindrome.checker("racecar"));
     }
 }
