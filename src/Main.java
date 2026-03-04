9e53089c00fe523e14c7de427bffae11f1e20925
Joanna Jacob <joannajacob910@gmail.com>
2026-03-04 22:10:13 +0530

    use case 13 done

--- src/Main.java
+++ src/Main.java
@@ -1,58 +1,44 @@
 //Author: Joanna Jacob
-// version 12.0
+// version 13.0
 
 import java.util.*;
 
-interface PalindromeStrategy {
-    boolean isPalindrome(String s);
-}
+public class Main {
 
-class StackStrategy implements PalindromeStrategy {
-    public boolean isPalindrome(String s) {
+    static boolean usingStack(String s) {
         Stack<Character> stack = new Stack<>();
         for (char c : s.toCharArray()) stack.push(c);
-
-        for (char c : s.toCharArray()) {
+        for (char c : s.toCharArray())
             if (c != stack.pop()) return false;
-        }
         return true;
     }
-}
 
-class DequeStrategy implements PalindromeStrategy {
-    public boolean isPalindrome(String s) {
-        Deque<Character> deque = new ArrayDeque<>();
-        for (char c : s.toCharArray()) deque.add(c);
-
-        while (deque.size() > 1) {
-            if (deque.pollFirst() != deque.pollLast()) return false;
-        }
+    static boolean usingTwoPointer(String s) {
+        int l = 0, r = s.length() - 1;
+        while (l < r)
+            if (s.charAt(l++) != s.charAt(r--)) return false;
         return true;
     }
-}
 
-class PalindromeChecker {
-    private PalindromeStrategy strategy;
-
-    PalindromeChecker(PalindromeStrategy strategy) {
-        this.strategy = strategy;
+    static long time(Runnable fn) {
+        long start = System.nanoTime();
+        fn.run();
+        return System.nanoTime() - start;
     }
 
-    void check(String s) {
-        System.out.println(s + " → " + (strategy.isPalindrome(s) ? "Palindrome" : "Not a Palindrome"));
-    }
-}
-
-public class Main {
     public static void main(String[] args) {
-        PalindromeChecker checker = new PalindromeChecker(new StackStrategy());
-        System.out.println("Stack: ");
-        checker.check("racecar");
-        checker.check("hello");
+        String s = "racecar";
 
-        checker = new PalindromeChecker(new DequeStrategy());
-        System.out.println("Deque: ");
-        checker.check("madam");
-        checker.check("world");
+        long start1 = System.nanoTime();
+        usingStack(s);
+        long t1 = System.nanoTime() - start1;
+
+        long start2 = System.nanoTime();
+        usingTwoPointer(s);
+        long t2 = System.nanoTime() - start2;
+
+        System.out.println("Stack       : " + t1 + " ns");
+        System.out.println("Two Pointer : " + t2 + " ns");
+        System.out.println("Faster      : " + (t1 < t2 ? "Stack" : "Two Pointer"));
     }
 }
